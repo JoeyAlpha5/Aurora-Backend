837abfd6021b207e4e374b69d13bbfceeb73a41a
Jalome <chjalome@gmail.com>
2020-12-31 07:43:45 +0200

    backend now running on new macos machine

--- AuroraVenv/Aurora/MobileApi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import getFeed, emptyParams,getLyrics
-
-urlpatterns = [
-    path('', emptyParams),
-    path('feed', getFeed),
-    path('lyrics', getLyrics),
-]
